01e1a336d35daf4795a937789b8d011f53b37bde
nourdawwa <110169499+nourdawwa@users.noreply.github.com>
2024-06-18 06:00:13 +0000

    Deploying to public from @ nourdawwa/portfoliotest1@34cded9b4b91a2c94f706c085b9865d26f8f01a1 🚀

---
+++ scripts/applygrained.js
@@ -0,0 +1,10 @@
+var options = {
+    "animate": false,
+    "patternWidth": 50,
+    "patternHeight": 50,
+    "grainOpacity": 0.05,
+    "grainDensity": 1,
+    "grainWidth": 1,
+    "grainHeight": 1
+  }
+  grained("#page", options);
